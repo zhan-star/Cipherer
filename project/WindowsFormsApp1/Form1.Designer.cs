ecb83739e570f3419544572b44cb603ceef10d7c
zhan-star <janik150802@gmail.com>
2020-11-04 10:04:38 +0600

    First commit

---
+++ project/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Cipherer
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cipherer));
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button6 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // saveFileDialog1
+            // 
+            resources.ApplyResources(this.saveFileDialog1, "saveFileDialog1");
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            resources.ApplyResources(this.openFileDialog1, "openFileDialog1");
+            // 
+            // richTextBox1
+            // 
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.richTextBox1_MouseMove);
+            // 
+            // richTextBox2
+            // 
+            resources.ApplyResources(this.richTextBox2, "richTextBox2");
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            this.richTextBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.richTextBox2_MouseMove);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button2_MouseMove);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button3_MouseMove);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button4_MouseMove);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            this.label2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label2_MouseMove);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.button5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button5_MouseMove);
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(22, 22);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            resources.ApplyResources(this.оПрограммеToolStripMenuItem, "оПрограммеToolStripMenuItem");
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // button6
+            // 
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Cipherer
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Cipherer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ project/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Cipherer : Form
+    {
+       
+        public Cipherer()
+        {
+            InitializeComponent();
+        }
+         
+    private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+                richTextBox1.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) {
+                richTextBox2.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Зашифровка текста
+            if (textBox1.Text != "")
+            {
+                // Установка шага шифрования
+                int num = Convert.ToInt32(textBox1.Text);
+
+                string a;
+
+                richTextBox2.Clear();
+                if (num < 20 && num > 0)
+                {
+                    for (int i = 0; i < richTextBox1.Lines.Length; i++)
+                    {
+                        a = richTextBox1.Lines[i];
+                        for (int j = 0; j < a.Length; j++)
+                        {
+                            int x = a[j];
+                            x = x + num;
+                            richTextBox2.AppendText(Convert.ToChar(x).ToString());
+
+                        }
+                        richTextBox2.AppendText("\n");
+                    }
+                    // Запись предыдущего шага
+                    label3.Text = Convert.ToString(num);
+                }
+                else {
+                    // Не допускаю шаг шифровки выше 20 для избежания ошибок
+                    MessageBox.Show("Шаг должен быть в диапазоне от 0 до 20.");
+                }
+            }
+            else {
+                int num = 3;
+                // Если оставить шаг пустым ...
+                string a;
+
+                richTextBox2.Clear();
+                if (num < 20 && num > 0)
+                {
+                    for (int i = 0; i < richTextBox1.Lines.Length; i++)
+                    {
+                        a = richTextBox1.Lines[i];
+                        for (int j = 0; j < a.Length; j++)
+                        {
+                            int x = a[j];
+                            x = x + num;
+                            richTextBox2.AppendText(Convert.ToChar(x).ToString());
+
+                        }
+                        richTextBox2.AppendText("\n");
+                    }
+                    label3.Text = Convert.ToString(3);
+                }
+                else {
+                    MessageBox.Show("Шаг должен быть в диапазоне от 0 до 20.");
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Расшифровка текста.
+            if (textBox1.Text != "")
+            {
+                int num = Convert.ToInt32(textBox1.Text);
+                string a;
+                richTextBox2.Clear();
+                for (int i = 0; i < richTextBox1.Lines.Length; i++)
+                {
+                    a = richTextBox1.Lines[i];
+                    for (int j = 0; j < a.Length; j++)
+                    {
+                        int x = a[j];
+                        x = x - num;
+                        richTextBox2.AppendText(Convert.ToChar(x).ToString());
+
+                    }
+                    richTextBox2.AppendText("\n");
+                }
+            }
+            else {
+                int num = 3;
+                string a;
+                richTextBox2.Clear();
+                for (int i = 0; i < richTextBox1.Lines.Length; i++)
+                {
+                    a = richTextBox1.Lines[i];
+                    for (int j = 0; j < a.Length; j++)
+                    {
+                        int x = a[j];
+                        x = x - num;
+                        richTextBox2.AppendText(Convert.ToChar(x).ToString());
+
+                    }
+                    richTextBox2.AppendText("\n");
+                }
+            }
+        }
+
+        private void button3_MouseMove(object sender, MouseEventArgs e)
+        {
+            label4.Text=("Зашифровать: зашифровать сообщение в верхнем окне.");
+        }
+
+        private void button4_MouseMove(object sender, MouseEventArgs e)
+        {
+            label4.Text = ("Расшифровать: расшифровать сообщение в верхнем окне.");
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label4.Text = ("Открыть: выбрать файл и открыть его.");
+        }
+
+        private void button2_MouseMove(object sender, MouseEventArgs e)
+        {
+            label4.Text = ("Сохранить: сохранить сообщение нижнего окна.");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Замена текстов местами
+            string upper = richTextBox1.Text;
+            string lower = richTextBox2.Text;
+            richTextBox1.Text = lower;
+            richTextBox2.Text = upper;
+        }
+
+        private void button5_MouseMove(object sender, MouseEventArgs e)
+        {
+            label4.Text = ("Поменять: поменять сообщения местами.");
+        }
+
+        private void richTextBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label4.Text = ("Первое окно: окно шифрования.");
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_MouseMove(object sender, MouseEventArgs e)
+        {
+            label4.Text = ("Второе окно: здесь находится зашифрованнный текст.");
+        }
+
+        private void label2_MouseMove(object sender, MouseEventArgs e)
+        {
+            label4.Text = ("Пред.шаг: последнее значение шага.");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label4.Text = ("Шаг шифровки: шаг шифровки сообщения.");
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Шифрование данных v0.1. Разработчик: Есенжолов Жангир, SA14/2-152.  2017-2018");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            ///Эта часть отвечает за невозможность ввода буквенных символов в строку.
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+        (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
